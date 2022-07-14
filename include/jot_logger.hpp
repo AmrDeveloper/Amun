af76be53949f27f6b6014cfc9c604091438b6df5
AmrDeveloper <amr96@programmer.net>
2022-07-15 01:08:23 +0200

    Improve jot logger to support 3 (info, debug, error) types and console colors

--- include/jot_logger.hpp
+++ include/jot_logger.hpp
@@ -1,23 +1,51 @@
 #pragma once
 
-#include <iostream>
+#include "jot_color.h"
+
+#include <unordered_map>
 
 #define JOT_LOGGER
 
 namespace jot {
 
-    struct JotLogger {
-       
+    enum LogKind {
+        Info,
+        Debug,
+        Error,
+    };
+
+    static std::unordered_map<LogKind, int> colors_map = {
+        {LogKind::Info, COLOR_YELLOW},
+        {LogKind::Debug, COLOR_BLUE},
+        {LogKind::Error, COLOR_RED},
+    };
+
+    class JotLogger {
+
+    public:
+        JotLogger& type(LogKind kind) {
+            text_color = colors_map[kind];
+            return *this;
+        }
+
         template<class T>
         JotLogger& operator<< ([[maybe_unused]] T content) {
 #ifdef JOT_LOGGER
-            std::cout << content;
+            material_print(content, text_color, background_color);
 #endif
             return *this;
         }
+
+    private:
+        int text_color = 0;
+        int background_color = 0;
     };
 
     static JotLogger logger;
-    #define logger logger << __FILE__ << ":" << __LINE__ << ':'
-}
 
+    #define logi logger.type(jot::LogKind::Info) << __FILE__ << ":" << __LINE__ << " i: "
+
+    #define logd logger.type(jot::LogKind::Debug) << __FILE__ << ":" << __LINE__ << " d: "
+
+    #define loge logger.type(jot::LogKind::Error) << __FILE__ << ":" << __LINE__ << " e: "
+}
