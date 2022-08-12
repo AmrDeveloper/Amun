54e403adfa31ecc6685e936e773ec48c741976f9
AmrDeveloper <amr96@programmer.net>
2022-08-12 19:39:52 +0200

    Fix clang code formatter python script source code

--- scripts/format_code.py
+++ scripts/format_code.py
@@ -5,7 +5,7 @@ file_paths = []
 for root, directories, files in os.walk("."):
     for filename in files:
         filepath = os.path.join(root, filename)
-        if filepath.endswith(('.h', '.hpp', '.c', '.cpp'))
+        if filepath.endswith(('.h', '.hpp', '.c', '.cpp')):
             file_paths.append(filepath)
 
 for file in file_paths:
