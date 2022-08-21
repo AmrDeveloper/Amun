5980e08660cb82be586c99cea4dc5432d73c6b94
AmrDeveloper <amr96@programmer.net>
2022-08-21 22:29:14 +0200

    Update Python scripts files to support nested files in the samples directory

--- scripts/compile_samples.py
+++ scripts/compile_samples.py
@@ -1,9 +1,16 @@
 import os
+from pathlib import Path
 
 extension = ".exe" if os == "nt" else ""
 executable = "./build/jot" + extension
 
-files = os.listdir("samples")
-for file in files:
-    command = executable + " compile samples/" + file
+def collect_all_files(path):
+    root = Path(path)
+    for p in root.rglob("*"):
+        if not p.is_file():
+            continue
+        yield p
+
+for file in collect_all_files("samples"):
+    command = executable + " compile " + str(file)
     os.system(command)
