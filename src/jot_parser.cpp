aeaa426ef5fd9b145c383c45f6ad8f9a7f322ed0
AmrDeveloper <amr96@programmer.net>
2022-09-05 20:41:54 +0200

    Add is Global flag when declaring field with type

--- src/jot_parser.cpp
+++ src/jot_parser.cpp
@@ -223,7 +223,7 @@ std::shared_ptr<FieldDeclaration> JotParser::parse_field_declaration(bool is_glo
         assert_kind(TokenKind::Equal, "Expect = after variable name.");
         auto value = parse_expression();
         assert_kind(TokenKind::Semicolon, "Expect semicolon `;` after field declaration");
-        return std::make_shared<FieldDeclaration>(name, type, value);
+        return std::make_shared<FieldDeclaration>(name, type, value, is_global);
     }
     assert_kind(TokenKind::Equal, "Expect = after variable name.");
     auto value = parse_expression();
