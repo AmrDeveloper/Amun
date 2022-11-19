92a4a4b498597533938e4c350ee2daa654b429bc
AmrDeveloper <amr96@programmer.net>
2022-11-19 14:58:59 +0200

    Change executable output directory and update scripts

--- scripts/check_samples.py
+++ scripts/check_samples.py
@@ -2,15 +2,26 @@ import os
 from pathlib import Path
 
 extension = ".exe" if os == "nt" else ""
-executable = "./build/bin/jot" + extension
+executable = "./jot" + extension
+samples_directory = "../samples"
 
+# Setup directory to be inside the executable directory
+current_directly = os.getcwd()
+if not current_directly.endswith('bin'):
+    current_directly += "/bin"
+    os.chdir(current_directly)
+
+# Collect all jot source files
 def collect_all_files(path):
     root = Path(path)
     for p in root.rglob("*"):
         if not p.is_file():
             continue
-        yield p
+        file_path = str(p)
+        if file_path.endswith(".jot"):
+            yield file_path
 
-for file in collect_all_files("samples"):
-    command = executable + " check " + str(file)
+# Check each jot file
+for file in collect_all_files(samples_directory):
+    command = executable + " check " + file
     os.system(command)
